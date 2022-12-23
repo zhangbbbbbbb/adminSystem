a020d9f95c5e0eae489bd7dc43afa2f3e81c8aa9
zhangbin <2362016211@qq.com>
2022-12-23 15:22:10 +0800

    初始化项目

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import { Button, Select } from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
+import router from './router/index.js'
+
+Vue.config.productionTip = false
+
+Vue.component(Button.name, Button)
+Vue.component(Select.name, Select)
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
