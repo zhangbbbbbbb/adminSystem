f3c62148b98d176d99fa2ea2088ac4ff04473bf5
zhangbin <2362016211@qq.com>
2022-12-26 18:05:29 +0800

    修改CommonAside

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -37,7 +37,8 @@
   export default {
     data() {
       return {
-        isCollapse: true
+        isCollapse: true,
+        menuList: []
       };
     },
     methods: {
@@ -46,7 +47,56 @@
       },
       handleClose(key, keyPath) {
         console.log(key, keyPath);
+      },
+      getMenuList(){
+        let data = [
+          {
+            path: '/',
+            name: 'home',
+            label: '首页',
+            icon:'s-home',
+            url: 'Home/Home'
+          },
+          {
+            path: '/mall',
+            name: 'mall',
+            label: '商品管理',
+            icon:'video-play',
+            url: 'MallManage/MallManage'
+          },
+          {
+            path: '/user',
+            name: 'user',
+            label: '用户管理',
+            icon:'user',
+            url: 'UserManage/userManage'
+          },
+          {
+            label: '其他',
+            icon: 'location',
+            children: [
+              {
+                path: '/page1',
+                name: 'page1',
+                label: '页面1',
+                icon:'setting',
+                url: 'other/Pageone'
+              },
+              {
+                path: '/page2',
+                name: 'page2',
+                label: '页面2',
+                icon:'setting',
+                url: 'other/Pageotwo'
+              }
+            ]
+          }
+        ]
+        return data
       }
+    },
+    mounted: function() {
+      this.menuList = this.getMenuList()
     }
   }
 </script>
